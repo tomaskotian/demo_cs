789d47ad90a4648cdce805521040c7e93b218aae
Tomas <11tomas16@gmail.com>
2023-02-25 22:09:16 +0100

    test conflict

--- 2d_shapes/Program/Program.cs
+++ 2d_shapes/Program/Program.cs
@@ -7,10 +7,13 @@ class Program
         Position position_ob1 = new Position(5, 3);
         Shape ob1 = new Shape(position_ob1);
         Console.WriteLine(ob1.Id);
+        Console.WriteLine(ob1.Id);
         Console.WriteLine(ob1.getPosition());
 
         Shape ob2 = new Shape();
         Console.WriteLine(ob2.Id);
         Console.WriteLine(ob2.getPosition());
+
+        const int x = 5;
     }
 }
