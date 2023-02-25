4ec9e520a1e3b57a3aa328e48b745533b25c3a54
Tomas <11tomas16@gmail.com>
2023-02-25 23:10:31 +0100

    pridal som class circle

--- 2d_shapes/Program/Shapes/Circle.cs
+++ 2d_shapes/Program/Shapes/Circle.cs
@@ -2,6 +2,13 @@
 
 class Circle : Shape
 {
-    int radius { get; }
-    int
+    const double pi = 3.14;
+    public int Radius { get; }
+    public double Area { get; }
+
+    public Circle(int radius, Position position) : base(position)
+    {
+        Radius = radius;
+        Area = Math.Round(radius * pi,2);
+    }
 }
