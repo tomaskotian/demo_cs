83f6a1d8f915e3b2870089cae5e3f7ead9cabaee
Ludovit <ludsce002@gmail.com>
2023-02-26 09:28:45 +0100

    pridanie Triangle este by som mal skusit aj tu struct line nejako toto som mal len take ze copy paste v podstate ale dobre na precvicenie aj ked teda bude to haluz ten projekt

--- 2d_shapes/Program/Program.cs
+++ 2d_shapes/Program/Program.cs
@@ -10,12 +10,12 @@ class Program
         Console.WriteLine(obj1.getPosition());
 
         Rectangle obj2 = new Rectangle(3, 5, Position.Default());
-        Console.WriteLine(obj2.SideA);
-        Console.WriteLine(obj2.SideB);
-        Console.WriteLine(obj2.AreaRec);
+        Console.WriteLine("{0}\n{1}\n{2}", obj2.SideA, obj2.SideB, obj2.AreaRec);
         Console.WriteLine(obj2.getPosition());
         
-
+        Triangle obj3 = new Triangle(4, 5, new (20, 10));
+        Console.WriteLine("Zakladna: {0}\nVyska: {1}\nPlocha: {2}", obj3.BaseT, obj3.Height, obj3.AreaTri);
+        Console.WriteLine("Suradnice: {0}", obj3.getPosition());
     }
     //main dopisanie kodu....
 }
--- 2d_shapes/Program/Shapes/Rectangle.cs
+++ 2d_shapes/Program/Shapes/Rectangle.cs
@@ -1,19 +1,17 @@
-﻿using System.Drawing;
-
-namespace Application;
+﻿namespace Application;
 
 class Rectangle : Shape
 {
     public int SideA { get; }
     public int SideB { get; }
 
-    public long AreaRec { get; }
+    public int AreaRec { get; }
 
     public Rectangle(int sideA, int sideB, Position position) : base(position)
     {
         SideA = sideA;
         SideB = sideB;
         AreaRec = sideA * sideB;
-        Console.WriteLine("\nRectangle:");
+        Console.WriteLine("\n*Rectangle*:");
     }
 }
